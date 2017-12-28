29d38afbef60ad8c60394f1d17f22f391186588b
wedwer666 <mitriucmaria@gmail.com>
2017-12-28 16:18:14 +0200

    Commit

---
+++ app/src/main/java/com/maria/medapp/WelcomeActivity.java
@@ -0,0 +1,34 @@
+package com.maria.medapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Maria on 28.12.2017.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    public Button butlog;
+
+    public void init()
+    {
+        butlog = (Button)findViewById(R.id.loginbtn);
+        butlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_activity);
+        init();
+    }
+}
